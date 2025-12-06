dc06d5be500a3cbec1b433023d8fc418748025da
Shahineze-Kadiri <Shahineze.kadiri@viacesi.fr>
2025-12-06 23:22:29 +0100

    Display.cpp MAJ

--- jeu_de_la_vie/src/Display.cpp
+++ jeu_de_la_vie/src/Display.cpp
@@ -1,10 +1,10 @@
 #include "../headers/Display.hpp"
 
 Display:: Display(const int cellSize, const int gridWidth,const int gridHeight){
-    this->cellSize = cellSize;
-    this->gridWidth = gridWidth;
-    this->gridHeight= gridHeight;
-    this->window(sf::VideoMode(gridWidth* cellSize, gridHeight* cellSize), "Game of Life");
+    cellSize(cellSize)
+    gridWidth (gridWidth);
+    gridHeight (gridHeight);
+    window(sf::VideoMode(gridWidth* cellSize, gridHeight* cellSize), "Game of Life");
 };
 
 
@@ -15,9 +15,9 @@ sf::RenderWindow& Display:: getWindow(){
 void Display:: renderGrid(const std::vector<std::vector<int>>& matrix){
     window.clear();
     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
-    for (x = 0; x < gridWidth; ++x) {
-        for (y = 0; y < gridHeight; ++y) {
-            if (grid[x][y] == 1) {
+    for (int x = 0; x < gridWidth; ++x) {
+        for (int y = 0; y < gridHeight; ++y) {
+            if (matrix[x][y] == 1) {
                 cell.setPosition(x * cellSize, y * cellSize);
                 window.draw(cell);
             }
